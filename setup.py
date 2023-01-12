86073b82e9100febdcc49a48e6094045caaf63b2
FdelMazo <FdelMazo@fi.uba.ar>
2023-01-12 11:10:40 -0300

    Globalize CARRERA and PADRON for every grafo

---
+++ setup.py
@@ -0,0 +1,8 @@
+# Carrera sobre la cual se hace el grafo
+# Puede ser cualquiera de:
+# sistemas informatica agrimensura alimentos civil electricista electronica 
+# industrial mecanica naval petroleo quimica sistemasviejo
+CARRERA = 'informatica'
+
+# Padron para los analisis particulares
+PADRON = '100029'
